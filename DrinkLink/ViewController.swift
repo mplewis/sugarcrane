a79516efe0935d92a8035a402614a03107190510
Matthew Lewis <matt@mplewis.com>
2016-01-10 22:31:50 -0600

    vc: replace images with buttons

--- DrinkLink/ViewController.swift
+++ DrinkLink/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wheel: UIButton!
+    @IBOutlet weak var beer: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func wheelTapped(sender: AnyObject) {
+        
+    }
 
-
+    @IBAction func beerTapped(sender: AnyObject) {
+        
+    }
 }
-
