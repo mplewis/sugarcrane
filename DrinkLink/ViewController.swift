f63d1f89d9d374005ac3cdbffee01d38a2688d72
Matthew Lewis <matt@mplewis.com>
2016-01-11 00:39:52 -0600

    bean: connect to beans

--- DrinkLink/ViewController.swift
+++ DrinkLink/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import SwiftHEXColors
 import Bean_iOS_OSX_SDK
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate {
 
     @IBOutlet weak var wheel: UIButton!
     @IBOutlet weak var beer: UIButton!
@@ -11,8 +11,14 @@ class ViewController: UIViewController {
     let orange = UIColor(hexString: "f39c12")
     let green = UIColor(hexString: "2ecc71")
     
+    var beanManager: PTDBeanManager?
+    var wheelBean: PTDBean?
+    var beerBean: PTDBean?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        beanManager = appDelegate.beanManager!
         beer.tintColor = red
         wheel.tintColor = red
     }
@@ -23,12 +29,50 @@ class ViewController: UIViewController {
         dest.senderSegue = segue.identifier
     }
     
+    func connectToBean(bean: PTDBean) {
+        bean.delegate = self
+        beanManager!.delegate = self
+        var error: NSError?
+        beanManager!.connectToBean(bean, error: &error)
+        if let e = error {
+            print(e)
+        }
+    }
+    
+    func connectToWheelBean(bean: PTDBean) {
+        wheelBean = bean
+        wheel.tintColor = orange
+        connectToBean(wheelBean!)
+    }
+    
+    func connectToBeerBean(bean: PTDBean) {
+        beerBean = bean
+        beer.tintColor = orange
+        connectToBean(beerBean!)
+    }
+    
     func wheelBeanSelected(bean: PTDBean) {
-        print("Wheel bean selected: \(bean.name)")
+        connectToWheelBean(bean)
     }
     
     func beerBeanSelected(bean: PTDBean) {
-        print("Beer bean selected: \(bean.name)")
+        connectToBeerBean(bean)
+    }
+    
+    func beanManager(beanManager: PTDBeanManager!, didConnectBean bean: PTDBean!, error: NSError!) {
+        if bean.identifier == wheelBean?.identifier {
+            wheel.tintColor = green
+        } else if bean.identifier == beerBean?.identifier {
+            beer.tintColor = green
+        }
+    }
+    
+    func beanManager(beanManager: PTDBeanManager!, didDisconnectBean bean: PTDBean!, error: NSError!) {
+        if bean.identifier == wheelBean?.identifier {
+            connectToWheelBean(bean)
+        } else if bean.identifier == beerBean?.identifier {
+            connectToBeerBean(bean)
+        }
     }
 
 }
