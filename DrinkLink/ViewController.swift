a2f60ae88c236746ae5550a254cce1c45512b35e
Matthew Lewis <matt@mplewis.com>
2016-01-11 01:18:00 -0600

    vc: working Bean to Bean

--- DrinkLink/ViewController.swift
+++ DrinkLink/ViewController.swift
@@ -39,13 +39,26 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
         }
     }
     
+    func disconnectFromBean(bean: PTDBean?) {
+        if bean == nil {
+            return
+        }
+        var error: NSError?
+        beanManager?.disconnectBean(bean!, error: &error)
+        if let e = error {
+            print(e)
+        }
+    }
+    
     func connectToWheelBean(bean: PTDBean) {
+        disconnectFromBean(wheelBean)
         wheelBean = bean
         wheel.tintColor = orange
         connectToBean(wheelBean!)
     }
     
     func connectToBeerBean(bean: PTDBean) {
+        disconnectFromBean(beerBean)
         beerBean = bean
         beer.tintColor = orange
         connectToBean(beerBean!)
@@ -60,6 +73,7 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
     }
     
     func beanManager(beanManager: PTDBeanManager!, didConnectBean bean: PTDBean!, error: NSError!) {
+        bean.delegate = self
         if bean.identifier == wheelBean?.identifier {
             wheel.tintColor = green
         } else if bean.identifier == beerBean?.identifier {
@@ -72,6 +86,28 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
             connectToWheelBean(bean)
         } else if bean.identifier == beerBean?.identifier {
             connectToBeerBean(bean)
+        }
+    }
+    
+    func bean(bean: PTDBean!, serialDataReceived raw: NSData!) {
+        if bean.identifier != beerBean?.identifier {
+            return
+        }
+        if let wb = wheelBean, rcvd = NSString(data: raw, encoding: NSUTF8StringEncoding) {
+            if rcvd.containsString("1") {
+                // drink was picked up
+                // Send a few commands; sometimes Bean doesn't wake up on the first one
+                wb.sendSerialString("1")
+                wb.sendSerialString("\n")
+                wb.sendSerialString("\n")
+                wb.sendSerialString("\n")
+            } else if rcvd.containsString("0") {
+                // drink was set down
+                wb.sendSerialString("0")
+                wb.sendSerialString("\n")
+                wb.sendSerialString("\n")
+                wb.sendSerialString("\n")
+            }
         }
     }
 
